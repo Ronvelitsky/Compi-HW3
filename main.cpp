6178add17a415ffcd5d19d0a95912889741f3a04
Ronvelitsky <94012697+Ronvelitsky@users.noreply.github.com>
2025-12-23 16:07:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,6 @@ int main() {
     yyparse();
 
     // Print the AST using the PrintVisitor
-    SemanticParser::SemanticParser visitor;
+    SemanticParser visitor;
     program->accept(visitor);
 }
